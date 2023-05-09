45d9ee8e2ecca921be1493fb5a9be8440b6cd64f
Dmitriy Bulgakov <4irtim@gmail.com>
2023-05-09 20:14:44 +0300

    fix: task 4 add remove dir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,6 +5,7 @@ async function copyDir(dirName) {
   const fullPath = path.join(__dirname, dirName);
   const newPath = path.join(__dirname, `${dirName}-copy`);
   const fileList = await fs.readdir(fullPath, { withFileTypes: true });
+  await fs.rm(newPath, { recursive: true, force: true });
   await fs.mkdir(newPath, { recursive: true });
   fileList.forEach((value) => {
     fs.copyFile(path.join(fullPath, value.name), path.join(newPath, value.name));
